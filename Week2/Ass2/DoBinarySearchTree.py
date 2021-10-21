924e7fbb6b90ff7e466269c605265517106fff91
ryu <taewookwon@doai.ai>
2021-10-21 15:42:30 +0900

    [Upload] Upload BST insert method.

--- Week2/Ass2/DoBinarySearchTree.py
+++ Week2/Ass2/DoBinarySearchTree.py
@@ -11,18 +11,18 @@ class DoBinarySearchTree(DoNode):
         self.do_root = None
 
     def do_insert(self, do_data):
-        self.do_root = self.do_insert_data(self.do_root, do_data)
+        self.do_root = self.__do_insert_data(self.do_root, do_data)
         return self.do_root is not None
 
-    def do_insert_data(self, do_node, do_data):
+    def __do_insert_data(self, do_node, do_data):
         if do_node is None:
             do_node = DoNode(do_data)
 
         else:
             if do_data <= do_node.do_data:
-                do_node.do_left = self.do_insert_data(do_node.do_left, do_data)
+                do_node.do_left = self.__do_insert_data(do_node.do_left, do_data)
             else:
-                do_node.do_left = self.do_insert_data(do_node.do_left, do_data)
+                do_node.do_left = self.__do_insert_data(do_node.do_left, do_data)
 
         return do_node
 
